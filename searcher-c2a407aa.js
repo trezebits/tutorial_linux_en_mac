2cc5b286056f56088978ea6119a18878c6029ee8
Félix C.E. <flxc33@gmail.com>
2026-04-24 07:51:07 +0200

    actualiz

--- searcher-c2a407aa.js
+++ searcher-c2a407aa.js
@@ -437,7 +437,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-432551e9.js',
+                path_to_root + 'searchindex-e37f205e.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
